c74c38bb615d048eb26634c048a833ddc2da7d62
Stanislav Ivochkin <isn@extrn.org>
2017-02-13 15:25:11 +0300

    Work around missing jcppy._version for local development

--- jcppy/__init__.py
+++ jcppy/__init__.py
@@ -3,6 +3,11 @@
 # Copyright (c) 2017 Stanislav Ivochkin <isn@extrn.org>
 # License: MIT (see LICENSE for details)
 
-from jcppy._version import __version__, __revision__
+try:
+    from jcppy._version import __version__, __revision__
+except ImportError:
+    __version__ = 'latest'
+    __revision__ = 'latest'
+    __hash__ = 'unknown'
 from jcppy.header import header
 from jcppy.source import source
